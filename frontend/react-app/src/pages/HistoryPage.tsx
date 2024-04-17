188e9f7de1cff30448029898ba5a0ecd4e93fe80
inu-k <inu6411e@gmail.com>
2024-04-18 03:08:38 +0900

    新しいAPIに対応 視聴履歴の表示を修正

--- frontend/react-app/src/pages/HistoryPage.tsx
+++ frontend/react-app/src/pages/HistoryPage.tsx
@@ -7,6 +7,8 @@ import { fetchData } from '../functions/utils';
 export default function HistoryPage() {
     const [historyProps, setHisotryProps] = useState<VideoHistoryProps>({ history: [] });
     const [date, setDate] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
     useEffect(() => {
         fetchData('http://localhost:8088/history')
@@ -17,11 +19,29 @@ export default function HistoryPage() {
                 console.error(error);
                 setHisotryProps({ history: [] });
             });
-        console.log('useEffect', historyProps);
     }, []);
 
-    const handleHistoryShow = () => {
-        fetchData(`http://localhost:8088/history/${date}`)
+    const handleHistoryShowOnDate = () => {
+        fetchData(`http://localhost:8088/history?date=${date}`)
+            .then(data => {
+                setHisotryProps({ history: data });
+            })
+            .catch(error => {
+                console.error(error);
+                setHisotryProps({ history: [] });
+            });
+    };
+
+    const handleHistoryShowOnDuration = () => {
+        var params: string[] = [];
+        if (startDate) {
+            params.push(`startDate=${startDate}`);
+        }
+        if (endDate) {
+            params.push(`endDate=${endDate}`);
+        }
+
+        fetchData(`http://localhost:8088/history?${params.join('&')}`)
             .then(data => {
                 setHisotryProps({ history: data });
             })
@@ -34,9 +54,15 @@ export default function HistoryPage() {
     return (
         <div>
             <h1>History</h1>
-            <p> 履歴を表示する日付を選択：
+            <p> 履歴を表示する特定の日付を選択：
                 <input type='date' value={date} onChange={(e) => setDate(e.target.value)} />
-                <button onClick={handleHistoryShow}>Show</button>
+                <button onClick={handleHistoryShowOnDate}>Show</button>
+            </p>
+            <p> 履歴を表示する期間を選択：
+                <input type='date' value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+                〜
+                <input type='date' value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+                <button onClick={handleHistoryShowOnDuration}>Show</button>
             </p>
             <HistoryList history={historyProps.history} />
         </div>
